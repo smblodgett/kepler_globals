trying to fix whatever the heck is still breaking the model normalization, plotting fixes and simplifying somewhat

src/kg_likelihood.py
@@ -99,9 +99,11 @@ def parametric_log_likelihood(params):
 
 
     # len_stellar_df = len(stellar_df)
+    print("params: ", params)
+
     Gamma0 = 10**params[0]
     grid_sum = 0.0
-    p_Period, Period_fine_grid, p_mass, mass_fine_grid,γ0,γ1,γ2,mass_break_1,mass_break_2,σ0,σ1,σ2,C, p_ecc, eccentricity_fine_grid, is_nan_in_pmfs, is_inf_in_pmfs = get_probability_distributions(params)
+    p_Period, Period_fine_grid, p_mass, mass_fine_grid,γ0,γ1,γ2,mass_break_1,mass_break_2,σ0,σ1,σ2,C, p_ecc, eccentricity_fine_grid, is_nan_in_pmfs, is_inf_in_pmfs, is_neg_in_pmfs = get_probability_distributions(params)
     
     # print(params)
     # print("get probability distribution time is ", time.time() - start_time)
@@ -109,11 +111,15 @@ def parametric_log_likelihood(params):
 
     if is_nan_in_pmfs: # If the pmfs are generated to contain NaN values, the parameters used to generate them are probably bad. Don't mess, just reject.
         print("nan in pmfs!")
-        return -np.inf
+        return -np.inf, {}, rank
     
     if is_inf_in_pmfs:
         print("inf in pmfs!")
-        return -np.inf
+        return -np.inf, {}, rank
+    
+    if is_neg_in_pmfs:
+        print("negative values in pmfs!")
+        return -np.inf, {}, rank
     
     synthetic_catalog, rng_metadata = generate_catalog(stellar_df,p_Period, Period_fine_grid, p_mass, mass_fine_grid, γ0,γ1,γ2,mass_break_1,mass_break_2,σ0,σ1,σ2,C, p_ecc, eccentricity_fine_grid,rank)
     ######## implement making sure that the random generated one 
@@ -125,7 +131,7 @@ def parametric_log_likelihood(params):
     local_voxel_grid = synthetic_catalog_to_grid(synthetic_catalog,voxel_grid)
 
     voxel_num_data = local_voxel_grid.likelihood_array[:,:,:,:,:,0]       # chat is saying to scale the model count by the number of phodymm systems that we have...? could this be right?
-    model_count = Gamma0 * local_voxel_grid.likelihood_array[:,:,:,:,:,1]
+    model_count = Gamma0 * local_voxel_grid.likelihood_array[:,:,:,:,:,1] 
 
     print("voxel_num_data.shape: ",voxel_num_data.shape)
     print("model_count.shape: ",model_count.shape)
@@ -137,15 +143,15 @@ def parametric_log_likelihood(params):
     print("num of model_count > 0:", len(model_count[model_count > 0]))
     print("shape of voxel_num_data > 0:", voxel_num_data[voxel_num_data > 0].shape)
     print("shape of model_count > 0:", model_count[model_count > 0].shape)
-    print("num of voxel_num_data > 1:", len(voxel_num_data[voxel_num_data > 1]))
-    print("num of model_count > 1:", len(model_count[model_count > 1]))
+    # print("num of voxel_num_data > 1:", len(voxel_num_data[voxel_num_data > 1]))
+    # print("num of model_count > 1:", len(model_count[model_count > 1]))
 
     if np.any((voxel_num_data < 0) | (np.isnan(voxel_num_data))):
         print("aaaaa")
-        return -np.inf
+        return -np.inf, rng_metadata, rank
     elif np.any((model_count < 0) | (np.isnan(model_count))):
         print("aaaaaaaaaaa")
-        return -np.inf
+        return -np.inf, rng_metadata, rank
     
 
     zero_mask = (model_count == 0) & (voxel_num_data == 0)

src/kg_plots.py
@@ -511,7 +511,7 @@ def param_analysis_plots(results_folder,model_run_folder,model_id,nburnin,nthinn
     print("max top log prob: ",max(top_log_prob))
     print("max log prob: ",max(log_prob))
 
-        # Sort by likelihood descending
+    # Sort by likelihood descending
     order = np.argsort(top_log_prob)[::-1]
     top_samples = top_samples[order]
     top_log_prob = top_log_prob[order]
@@ -527,42 +527,16 @@ def param_analysis_plots(results_folder,model_run_folder,model_id,nburnin,nthinn
     rank = rng_metadata["rank"]
     time_seed = rng_metadata["time_seed"]
 
-    Γ0 = 10**top_samples[:,0]
-    γ0 = top_samples[:,1]
-    γ1 = top_samples[:,2]
-    γ2 = top_samples[:,3]
-    σ0 = top_samples[:,4]
-    σ1 = top_samples[:,5]
-    σ2 = top_samples[:,6]
-    mass_break_1 = top_samples[:,7]
-    mass_break_2 = top_samples[:,8]
-    C = top_samples[:,9]
-    μM = top_samples[:,10]
-    σM = top_samples[:,11]
-    β1 = top_samples[:,12]
-    β2 = top_samples[:,13]
-    β3 = top_samples[:,14]
-    Period_break_1 = top_samples[:,15]
-    Period_break_2 = top_samples[:,16]
-    α = top_samples[:,17]
-    λ = top_samples[:,18]
-    σ_e = top_samples[:,19]
-
-
-    print("model_params: ", model_params)
-    print("log10(Gamma0) model_params[0]: ", model_params[0])
-    print("Γ0: ",Γ0)
-    print("top samples: ",top_samples)
-
-    Gamma0 = 10**model_params[0]
+    print("top_samples[0]: ",top_samples[0])
+    print("model_params: ",model_params)
 
     
-    p_Period, Period_fine_grid, p_mass, mass_fine_grid,γ0,γ1,γ2,mass_break_1,mass_break_2,σ0,σ1,σ2,C, p_ecc, eccentricity_fine_grid, is_nan_in_pmfs, is_inf_in_pmfs = get_probability_distributions(model_params)
+    p_Period, Period_fine_grid, p_mass, mass_fine_grid,γ0,γ1,γ2,mass_break_1,mass_break_2,σ0,σ1,σ2,C, p_ecc, eccentricity_fine_grid, is_nan_in_pmfs, is_inf_in_pmfs, is_neg_in_pmfs = get_probability_distributions(top_samples[0])
     synthetic_catalog, rng_metadata = generate_catalog(stellar_df,p_Period, Period_fine_grid, p_mass, mass_fine_grid, γ0,γ1,γ2,mass_break_1,mass_break_2,σ0,σ1,σ2,C, p_ecc, eccentricity_fine_grid,rank,master_seed,time_seed)
     voxel_grid = synthetic_catalog_to_grid(synthetic_catalog,voxel_grid)
 
     voxel_num_data = voxel_grid.likelihood_array[:,:,:,:,:,0]
-    model_count = Γ0[0] * voxel_grid.likelihood_array[:,:,:,:,:,1]
+    model_count = 10**top_samples[0,0] * voxel_grid.likelihood_array[:,:,:,:,:,1] # check for 
 
     def rayleigh_exponential(alpha,lamb,sigma,e):
         return (alpha*((lamb*np.exp(-lamb*e))/(1-np.exp(-lamb))) + 
@@ -1039,11 +1013,11 @@ def main(voxel_id,plottype,model_run_folder_argv):
     heatmap_plot_type = plotprops.get("heatmap_plot_type")
     residual_plot_type = plotprops.get("residual_plot_type")
     model_id = plotprops.get("model_id")
-    param_result_filename = plotprops.get("param_result_filename")
+    param_result_filename = plotprops.get("param_result_filename") + f'_{model_id}.h5'
     model_run_folder = plotprops.get("model_run_folder") 
     voxel_grid_json_object_filename = plotprops.get("voxel_json_filename")
 
-    best_guess_filename = plotprops["best_guess_filename"] + f'_{model_id}.json'
+    best_guess_filename = plotprops["best_guess_filename"] + f'model_{model_id}/best_fit.json'
 
     
     if model_run_folder_argv is not None:

src/kg_probability_distributions.py
@@ -445,9 +445,12 @@ def get_probability_distributions(params):
     if (is_inf_in_pmfs := (not np.isfinite(p_ecc).any() or not np.isfinite(p_Period).any() or not np.isfinite(p_mass).any())):
         print("Warning: PMFs contain inf. This parameter draw is bad, let's skip it!")
 
+    if (is_neg_in_pmfs := (np.any(p_ecc < 0) or np.any(p_Period < 0) or np.any(p_mass < 0))):
+        print("Warning: PMFs contain negative values. This parameter draw is bad, let's skip it!")
 
 
-    return p_Period, Period_fine_grid, p_mass, mass_fine_grid,γ0,γ1,γ2,mass_break_1,mass_break_2,σ0,σ1,σ2,C, p_ecc, eccentricity_grid, is_nan_in_pmfs, is_inf_in_pmfs
+
+    return p_Period, Period_fine_grid, p_mass, mass_fine_grid,γ0,γ1,γ2,mass_break_1,mass_break_2,σ0,σ1,σ2,C, p_ecc, eccentricity_grid, is_nan_in_pmfs, is_inf_in_pmfs, is_neg_in_pmfs
 
 
 def normalize_pdf_to_pmf(pdf, grid):
@@ -478,7 +481,7 @@ def synthetic_catalog_to_grid(synthetic_catalog, voxel_grid):
     # print("synthetic catalog count: ", np.sum(synthetic_catalog))
     # originally synthetic catalog is in order period, mass, radius, ecc, omega confirmed 12/19 that this is working right
     synthetic_catalog = synthetic_catalog[:, [2, 0, 1, 3, 4]]
-    print("rearranged catalog shape: ", synthetic_catalog.shape)
+    # print("rearranged catalog shape: ", synthetic_catalog.shape)
     # print("rearranged synthetic catalog head: ", synthetic_catalog[:5,:])
 
     # print("rearranged catalog: ", synthetic_catalog)
@@ -508,12 +511,12 @@ def synthetic_catalog_to_grid(synthetic_catalog, voxel_grid):
     # p_d = voxel_grid.p_detection_interp(synthetic_catalog)
     # p_t = voxel_grid.p_transit_interp(synthetic_catalog)
     # completeness = p_d * p_t
-    print("synthetic catalog shape after filter: ", synthetic_catalog.shape)
+    # print("synthetic catalog shape after filter: ", synthetic_catalog.shape)
     
 
     completeness = voxel_grid.completeness_interp(synthetic_catalog)
-    print("completeness shape: ", completeness.shape)
-    print("completeness head: ", completeness[:5])
+    # print("completeness shape: ", completeness.shape)
+    # print("completeness head: ", completeness[:5])
     return pack_points_vectorized(synthetic_catalog,voxel_grid,completeness)
     # return pack_points_fast(synthetic_catalog,voxel_grid,completeness)
 
@@ -568,40 +571,40 @@ def pack_points_vectorized(cat, voxel_grid, completeness):
     e_idx = e_idx[valid]; o_idx = o_idx[valid]
     w = completeness[valid]
 
-    print("r_idx shape: ", r_idx.shape)
-    print("p_idx shape: ", p_idx.shape)
-    print("m_idx shape: ", m_idx.shape)
-    print("e_idx shape: ", e_idx.shape)
-    print("o_idx shape: ", o_idx.shape)
-    print("w shape: ", w.shape)
-    print("w: ", w)
+    # print("r_idx shape: ", r_idx.shape)
+    # print("p_idx shape: ", p_idx.shape)
+    # print("m_idx shape: ", m_idx.shape)
+    # print("e_idx shape: ", e_idx.shape)
+    # print("o_idx shape: ", o_idx.shape)
+    # print("w shape: ", w.shape)
+    # print("w: ", w)
 
     # flatten the multi-index to 1D
     shape = (voxel_grid.r_len, voxel_grid.p_len, voxel_grid.m_len,
              voxel_grid.e_len, voxel_grid.o_len)
     flat_idx = np.ravel_multi_index((r_idx, p_idx, m_idx, e_idx, o_idx), shape)
 
-    print("flat_idx shape: ", flat_idx.shape)
-    print("flat_idx head: ", flat_idx[:5])
+    # print("flat_idx shape: ", flat_idx.shape)
+    # print("flat_idx head: ", flat_idx[:5])
     # sum weights per flat index
     total_voxels = np.prod(shape)
     sums = np.bincount(flat_idx, weights=w, minlength=total_voxels)
 
-    print("sum(sums): ", np.sum(sums))
+    # print("sum(sums): ", np.sum(sums))
 
     # reshape and add into likelihood array's last index (1)
     sums = sums.reshape(shape)
 
-    print("reshaped sum(sums): ", np.sum(sums))
+    # print("reshaped sum(sums): ", np.sum(sums))
 
     # assumes likelihood_array[..., 1] exists and matches shape
     voxel_grid.likelihood_array[:,:,:,:,:, 1] = sums
 
     model_count = voxel_grid.likelihood_array[:,:,:,:,:,1]
 
-    print("sum of voxel grid model count: ", np.sum(voxel_grid.likelihood_array[:,:,:,:,:,1]))
-    print("num of model_count > 0 inside pack_points: ", len(model_count[model_count > 0]))
-    print("num of model_count > 1 inside pack_points: ", len(model_count[model_count > 1]))
+    # print("sum of voxel grid model count: ", np.sum(voxel_grid.likelihood_array[:,:,:,:,:,1]))
+    # print("num of model_count > 0 inside pack_points: ", len(model_count[model_count > 0]))
+    # print("num of model_count > 1 inside pack_points: ", len(model_count[model_count > 1]))
 
     return voxel_grid
 